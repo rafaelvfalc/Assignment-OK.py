283085cf5aa863ce82ed5ad524029da2dceafefc
Hector Medeiros <hector.medeiros@ccc.ufcg.edu.br>
2017-07-18 03:32:04 -0300

    Commiting assignment

---
+++ hw02_header.py
@@ -0,0 +1,17 @@
+﻿HW_SOURCE_FILE = 'hw02.py'
+from functools import reduce
+
+def square(x):
+    return x * x
+
+def triple(x):
+    return 3 * x
+
+def identity(x):
+    return x
+
+def increment(x):
+    return x + 1
+
+from operator import add, mul
+
